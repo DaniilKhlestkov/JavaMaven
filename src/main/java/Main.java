b4c389d6813d509eefc4ae843973fc28076f477f
DaniilKhlestkov <khlestkov00@inbox.ru>
2023-10-28 23:39:27 +0300

    created maven

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет");
-        System.out.println("push");
+        System.out.println("pus");
 
     }
 }
